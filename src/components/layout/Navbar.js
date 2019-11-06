3e9cc6273490949323fe20fc54cd9e01b6683081
Jeff <zyeelejeff@gmail.com>
2019-11-07 06:04:37 +0800

    Portal Set Up

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,10 +4,10 @@ import { Link  } from 'react-router-dom';
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+            <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
-                        Register UI
+                        Sign-In-Portal
           </Link>
                     <button
                         className="navbar-toggler"
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
 
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+export default withAuth(class Home extends Component {
+    state = { authenticated: null };   
+
+        checkAuthentication = async() => {
+        const authenticated = await this.props.auth.isAuthenticated();
+        if (authenticated !== this.state.authenticated) {
+            this.setState({ authenticated });
+        }
+    }
+
+    async componentDidMount() {
+        this.checkAuthentication();
+    }
+
+    async componentDidUpdate() {
+        this.checkAuthentication();
+    }
+
+        login = async() => {
+        this.props.auth.login('/');
+    }
+
+        logout = async() => {
+        this.props.auth.logout('/');
+    }
+
+    render() {
+        if (this.state.authenticated === null) return null;
+
+        const mainContent = this.state.authenticated ? (
+          <div>
+          <p className="lead">Complete! To See Welcome Message, <Link to="/users">
+              Click Here</Link></p>
+          <button className="btn btn-light btn-lg" onClick={this.logout}>Logout</button>
+          </div>
+        ) : ( 
+            <div> 
+            {/* <p className="lead">Sign Up Here <Link to="/users">
+            Click Here</Link></p> */}
+            <button className="btn btn-dark btn-lg" onClick={this.login}>Sign In</button>
+            </div>
+        );
+
+        return (
+            <div className="jumbotron">
+                <h1 className="display-4">Sign In Portal</h1>
+            {mainContent}
+            </div>
+        );
+    }  
 }
-
-export default Home;
+);
